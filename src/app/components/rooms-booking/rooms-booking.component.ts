921ec6806b2169a62d3842881a112d1b90bff29e
Supun <nissankasupunrasanga@gmail.com>
2022-07-01 17:27:00 +0530

    Initial Commit

--- src/app/components/rooms-booking/rooms-booking.component.ts
+++ src/app/components/rooms-booking/rooms-booking.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { BookingService } from 'src/app/services/booking.service';
 
 @Component({
@@ -8,27 +10,117 @@ import { BookingService } from 'src/app/services/booking.service';
 })
 export class RoomsBookingComponent implements OnInit {
 
-  submitted !:boolean;
-  formControls = this.BookingService.form.controls;
-  showSuccessMessage !: boolean;
+  loading:BehaviorSubject<boolean>=new BehaviorSubject<boolean>(false);
+  loading$!:Observable<boolean>;
+
+  // submitted !:boolean;
+  // formControls = this.BookingService.form.controls;
+  // showSuccessMessage !: boolean;
+
+  customForm!:FormGroup
+  customerList: any[] = [];
+  isUpdate:boolean=false;
+  selectedId!:string;
 
   constructor(
-    public BookingService : BookingService
+    public BookingService : BookingService,
+    private fb: FormBuilder,
   ) { }
 
   ngOnInit(): void {
+    //this.loading$=this.loading.asObservable();
+    this.initForm();
+    this.getList();
   }
-  onSubmit(){
-    this.submitted=true;
-    if(this.BookingService.form.valid){
-      if (this.BookingService.form.get('$key')?.value == null)
-        this.BookingService.insertBooking(this.BookingService.form.value);
-      // else
-      // this.BookingService.updateBooking(this.BookingService.form.value);
-        this.showSuccessMessage = true;
-        setTimeout(()=>this.showSuccessMessage=false,3000);
-      this.submitted=false;
-      this.BookingService.form.reset();
+  // onSubmit(){
+  //   this.submitted=true;
+  //   if(this.BookingService.form.valid){
+  //     if (this.BookingService.form.get('$key')?.value == null)
+  //       this.BookingService.insertBooking(this.BookingService.form.value);
+  //     // else
+  //     // this.BookingService.updateBooking(this.BookingService.form.value);
+  //       this.showSuccessMessage = true;
+  //       setTimeout(()=>this.showSuccessMessage=false,3000);
+  //     this.submitted=false;
+  //     this.BookingService.form.reset();
+  //   }
+  // }
+
+  initForm(): void {
+    this.customForm = this.fb.group({
+      $key: [],
+    checkIn: ['',Validators.required],
+    checkOut: ['',Validators.required],
+    NoRooms: ['',Validators.required],
+    RoomType: ['',Validators.required],
+    Adult: ['',Validators.required],
+    Childern: ['',Validators.required],
+    FirstName: ['',Validators.required],
+    LastName: ['',Validators.required],
+    email: ['',Validators.email],
+    ContactNumber: ['',Validators.required,Validators.minLength(9)],
+    });
+  }
+
+    onSaveOrUpdate(): void {
+      if (this.customForm.invalid){
+        alert('Please fill required fields');
+        return;
     }
+    this.loading.next(true);
+
+    if(this.isUpdate){
+  //Update Record
+  this.BookingService.update(this.customForm.value,this.selectedId).subscribe(res=>{
+         this.getList();
+         this.loading.next(false);
+         alert('Record has been updated.');
+
+  });
+
+    }else{
+  //Save Record
+      this.BookingService.create(this.customForm.value).subscribe(res=>{
+        this.customForm.reset();
+        this.getList();
+        this.loading.next(false);
+      },error =>{
+        alert('Error occured when saving data.\n' + error);
+      },()=>{
+        console.log('completed');
+      })
+      }
+    }
+      getList():void{
+        this.BookingService.getAll().subscribe(res => {
+          this.customerList = res;
+        } );
+      }
+      onUpdate(customer:any):void{
+        this.isUpdate=true,
+        //this.selectedId=customer.id,
+
+        this.customForm.patchValue({
+          firstName:customer.firstName,
+          lastname:customer.lastName,
+          dob:customer.dob,
+          contactNo:customer.contactNo,
+          address:customer.address
+        });
+      }
+
+  onDelete(id:string):void{
+    let isConfirm : boolean=confirm('Are you want to delete this Record?');
+
+    if(isConfirm){
+      this.BookingService.delete(id).subscribe(res =>{
+        console.log(res);
+        this.getList();
+    })
   }
+  }
+
+
+
+
 }
--- src/app/services/booking.service.ts
+++ src/app/services/booking.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { AngularFireDatabase ,AngularFireList} from '@angular/fire/compat/database'
+import { environment } from 'src/environments/environment';
+import { map, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,61 +11,92 @@ import { AngularFireDatabase ,AngularFireList} from '@angular/fire/compat/databa
 export class BookingService {
 
   constructor(
-    private Fb: AngularFireDatabase
+    private http: HttpClient
   ) { }
-    bookingList !: AngularFireList<any>;
 
-  form = new FormGroup({
-    $key: new FormControl(),
-    checkIn: new FormControl('',Validators.required),
-    checkOut: new FormControl('',Validators.required),
-    NoRooms: new FormControl('',Validators.required),
-    RoomType: new FormControl('',Validators.required),
-    Adult: new FormControl('',Validators.required),
-    Childern: new FormControl('',Validators.required),
-    FirstName: new FormControl('',Validators.required),
-    LastName: new FormControl('',Validators.required),
-    email: new FormControl('',Validators.email),
-    ContactNumber: new FormControl('',[Validators.required,Validators.minLength(9)]),
-  });
 
-  getBooking(){
-    this.bookingList=this.Fb.list('booking');
-    return this.bookingList.snapshotChanges();
-  }
-  insertBooking(booking: any){
-    this.bookingList.push({
-    checkIn:booking.checkIn,
-    checkOut:booking.checkOut,
-    NoRooms: booking.NoRooms,
-    RoomType: booking.RoomType,
-    Adult: booking.Adult,
-    Childern: booking.Childern,
-    FirstName: booking.FirstName,
-    LastName: booking.LastName,
-    email: booking.email,
-    ContactNumber: booking.ContactNumber
-    });
-  }
-  populateForm(booking:any){
-    this.form.setValue(booking);
-  }
-  updateBooking(booking:any){
-    this.bookingList.update(booking.$key,{
-    checkIn:booking.checkIn,
-    checkOut:booking.checkOut,
-    NoRooms: booking.NoRooms,
-    RoomType: booking.RoomType,
-    Adult: booking.Adult,
-    Childern: booking.Childern,
-    FirstName: booking.FirstName,
-    LastName: booking.LastName,
-    email: booking.email,
-    ContactNumber: booking.ContactNumber
-    });
+  // form = new FormGroup({
+  //   $key: new FormControl(),
+  //   checkIn: new FormControl('',Validators.required),
+  //   checkOut: new FormControl('',Validators.required),
+  //   NoRooms: new FormControl('',Validators.required),
+  //   RoomType: new FormControl('',Validators.required),
+  //   Adult: new FormControl('',Validators.required),
+  //   Childern: new FormControl('',Validators.required),
+  //   FirstName: new FormControl('',Validators.required),
+  //   LastName: new FormControl('',Validators.required),
+  //   email: new FormControl('',Validators.email),
+  //   ContactNumber: new FormControl('',[Validators.required,Validators.minLength(9)]),
+  // });
+
+  // getBooking(){
+  //   this.bookingList=this.Fb.list('booking');
+  //   return this.bookingList.snapshotChanges();
+  // }
+  // insertBooking(booking: any){
+  //   this.bookingList.push({
+  //   checkIn:booking.checkIn,
+  //   checkOut:booking.checkOut,
+  //   NoRooms: booking.NoRooms,
+  //   RoomType: booking.RoomType,
+  //   Adult: booking.Adult,
+  //   Childern: booking.Childern,
+  //   FirstName: booking.FirstName,
+  //   LastName: booking.LastName,
+  //   email: booking.email,
+  //   ContactNumber: booking.ContactNumber
+  //   });
+  // }
+  // populateForm(booking:any){
+  //   this.form.setValue(booking);
+  // }
+  // updateBooking(booking:any){
+  //   this.bookingList.update(booking.$key,{
+  //   checkIn:booking.checkIn,
+  //   checkOut:booking.checkOut,
+  //   NoRooms: booking.NoRooms,
+  //   RoomType: booking.RoomType,
+  //   Adult: booking.Adult,
+  //   Childern: booking.Childern,
+  //   FirstName: booking.FirstName,
+  //   LastName: booking.LastName,
+  //   email: booking.email,
+  //   ContactNumber: booking.ContactNumber
+  //   });
+  // }
+
+  // deleteBooking($key:string){
+  //   this.bookingList.remove($key);
+  // }
+  create(customer: any): Observable<any>{
+    //return this.http.post(`${this.baseAPIUrl}/customer.json`,customer);
+    // const headers : HttpHeaders =  new HttpHeaders()
+    // .set('content-type', 'application/json')
+    // .set('Access-Control-Allow-Origin', '*')
+    return this.http.post(`${environment.baseAPIUrl}/customer.json`,customer);
   }
 
-  deleteBooking($key:string){
-    this.bookingList.remove($key);
+  getAll(): Observable<any>{
+    //return this.http.get(`${this.baseAPIUrl}/customer.json`).pipe(map((res) => {
+    return this.http.get(`${environment.baseAPIUrl}/customer.json`).pipe(map((res) => {
+      const custom:any[] = [];
+      for(const key in res){
+        if(res.hasOwnProperty(key)){
+          custom.push({...res[$key], id: key});
+        }
+      }
+      return custom;
+    }));
   }
+
+  update(customer: any, id: string): Observable<any>{
+    //return this.http.put(`${this.baseAPIUrl}/customer/${id}.json`,customer);
+    return this.http.put(`${environment.baseAPIUrl}/customer/${id}.json`,customer);
+  }
+
+  delete(id: string): Observable<any>{
+    //return this.http.delete(`${this.baseAPIUrl}/customer/${id}.json`);
+    return this.http.delete(`${environment.baseAPIUrl}/customer/${id}.json`);
+  }
+
 }
